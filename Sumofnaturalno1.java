8bcea9fdd10244e63532d017a9740471dac7874f
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-10 19:51:14 +0530

    Add files via upload

---
+++ Sumofnaturalno1.java
@@ -0,0 +1,17 @@
+class Sumofnaturalno1 
+{
+    public static void main(String[] args)   
+    {  
+    int num = 100, i = 1, sum = 0;  
+    //executes until the condition returns true  
+    while(i <= num)  
+    {  
+    //adding the value of i into sum variable  
+    sum = sum + i;  
+    //increments the value of i by 1  
+    i++;  
+    }  
+    //prints the sum   
+    System.out.println("Sum of First 100 Natural Numbers is = " + sum);  
+    }      
+}
