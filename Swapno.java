9b1352f9feccb0da8fe1c0043864b7f2623631bb
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-11 19:48:16 +0530

    Add files via upload

---
+++ Swapno.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Swapno 
+{
+   public static void main(String[] args) {  
+       int x, y, t;// x and y are to swap   
+       Scanner sc = new Scanner(System.in);  
+       System.out.println("Enter the value of X and Y");  
+       x = sc.nextInt();  
+       y = sc.nextInt();  
+       System.out.println("before swapping numbers: "+x +"  "+ y);  
+       /*swapping */  
+       t = x;  
+       x = y;  
+       y = t;  
+       System.out.println("After swapping: "+x +"   " + y);  
+       System.out.println( ); 
+       sc.close(); 
+    }    
+}
