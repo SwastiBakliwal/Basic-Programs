26c1759daf567e6ac6dc764034375b5fb0472f95
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-08-11 19:42:32 +0530

    Add files via upload

---
+++ Sumofnaturalno2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Sumofnaturalno2 
+{
+  public static void main(String[] args)   
+{     
+int num, i, sum = 0;  
+//object of Scanner class  
+Scanner sc = new Scanner(System.in);  
+System.out.print("Sum from: ");  
+//takes an integer as input  
+i = sc.nextInt();  
+System.out.print("Sum up to: ");  
+//takes an integer as input  
+num = sc.nextInt();  
+while(i <= num)  
+{  
+//adding the value of i into sum variable  
+sum = sum + i;  
+//increments the value of i by 1  
+i++;  
+}  
+//prints the sum  
+System.out.println("Sum of Natural Numbers = " + sum);
+sc.close();  
+}  
+}      
+
