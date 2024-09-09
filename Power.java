dc53d2112d683c9ea1ac8979379242760aad4797
SwastiBakliwal <bakliwalswasti@gmail.com>
2024-09-09 19:47:04 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class PowerOfNumber {
+
+    public static void main(String[] args) {
+   
+    Scanner input = new Scanner(System.in);
+   
+    System.out.print("Enter base: ");
+   
+    int base = input.nextInt();
+   
+    System.out.print("Enter exponent: ");
+   
+    int exponent = input.nextInt();
+   
+    long result = 1;
+   
+    while (exponent != 0) {
+   
+    result *= base;
+   
+    --exponent;
+   
+    }
+   
+    System.out.println("Result: " + result);
+   
+    input.close();
+   
+    }
+   
+   }
